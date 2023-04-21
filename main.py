14d740d8f2e25d2bdc6ddbfe06eddf9d81c7d384
pyrometheous <Pyrometheous@gmail.com>
2023-04-21 11:23:57 -0700

    Made some big changes for how the GUI apps are created

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
     application_name_03 = "Format SD Card"
 
     def organize_files(operation):
-        folder_path = path_label.cget("text")
+        folder_path = chd_path_label.cget("text")
 
         if operation == "generate_m3u":
             success, message = rom_file_organizer.generate_m3u_playlist_from_CHD(
@@ -32,16 +32,11 @@ def main():
         else:
             messagebox.showerror("Error", message)
 
-    def browse_directory():
+    def browse_directory(app_button, app_path_label):
         folder_path = filedialog.askdirectory()
-
         if folder_path:
-            path_label.config(text=folder_path)
-            path_label_sd_card.config(text=folder_path)  # Add this line to update the path_label_sd_card text
-            organize_button1.config(state=tk.NORMAL)
-            organize_button2.config(state=tk.NORMAL)
-            setup_sd_card_button_garlic.config(state=tk.NORMAL)
-            setup_sd_card_button_batocera.config(state=tk.NORMAL)
+            app_path_label.config(text=folder_path)
+            app_button.config(state=tk.NORMAL)
 
     def prompt_relaunch_with_admin_rights():
         root = tk.Tk()
@@ -74,20 +69,23 @@ def main():
         update_right_frame(app_name)
 
     def update_right_frame(app_name):
-        if app_name == application_name_01:
+        if app_name == application_name_01: # Organize CHD Files
             chd_frame.grid(column=1, row=1, rowspan=2, sticky=(tk.W, tk.E, tk.N, tk.S))
             sd_card_frame.grid_remove()
-        elif app_name == application_name_02:
+        elif app_name == application_name_02: # Setup ROM SD Card
             chd_frame.grid_remove()
             sd_card_frame.grid(column=1, row=1, rowspan=2, sticky=(tk.W, tk.E, tk.N, tk.S))
+        elif app_name == application_name_03: # Format SD Card
+            chd_frame.grid_remove()
+            sd_card_frame.grid_remove()
         else:
             chd_frame.grid_remove()
             sd_card_frame.grid_remove()
 
-    def setup_sd_card(os):
-        folder_path = path_label.cget("text")
+    def setup_sd_card(operating_system):
+        folder_path = chd_path_label.cget("text")
 
-        success, message = sd_card_formatter.create_rom_folders(folder_path, os)
+        success, message = sd_card_formatter.create_rom_folders(folder_path, operating_system)
 
         if success:
             messagebox.showinfo("Completed", message)
@@ -140,6 +138,117 @@ def main():
 
         return application_names
 
+    def create_app_frame_template():
+        frame = ttk.Frame(right_frame)
+
+    def populate_apps():
+        listbox = tk.Listbox(left_frame, height=len(application_names),
+                                  width=calculate_pixel_width(application_names))
+        for i, application_name in enumerate(application_names):
+            listbox.insert(i, application_name)
+        listbox.bind(
+            "<<ListboxSelect>>",
+            lambda event: on_click(listbox.get(listbox.curselection())),
+        )
+        listbox.grid(column=0, row=1)
+
+        r_frame = ttk.Frame(window, relief="groove", borderwidth=1)
+        r_frame.grid(column=1, row=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+
+        title = ttk.Label(r_frame, text="")
+        title.grid(column=0, row=0, columnspan=2, sticky=(tk.W, tk.E))
+        return listbox, r_frame, title
+
+    def enable_buttons_after_browsing(app_path_label, *buttons):
+        def inner_enable_buttons_after_browsing():
+            folder_path = browse_directory(buttons[0], app_path_label)
+            for button in buttons:
+                button.config(state=tk.NORMAL)
+
+        return inner_enable_buttons_after_browsing
+
+
+    def create_organize_chd_files_frame():
+        frame = ttk.Frame(right_frame)
+        frame.columnconfigure(1, weight=1)
+
+        # Create Generate M3U Playlists button
+        organize_button1 = ttk.Button(
+            frame,
+            text="Generate M3U Playlists",
+            state=tk.DISABLED,
+            command=lambda: organize_files("generate_m3u"),
+        )
+        organize_button1.grid(column=0, row=3, sticky=(tk.W, tk.E))
+
+        # Create Revert to Single Folder button
+        organize_button2 = ttk.Button(
+            frame,
+            text="Revert to Single Folder",
+            state=tk.DISABLED,
+            command=lambda: organize_files("revert"),
+        )
+        organize_button2.grid(column=1, row=3, sticky=(tk.W, tk.W))
+
+        # Setup Path Label
+        path_label = ttk.Label(frame, text="")
+        path_label.grid(column=1, row=2, sticky=(tk.W, tk.E))
+
+        # Setup Browse Button
+        browse_button = ttk.Button(frame, text="Browse", command=lambda: browse_directory(organize_button1, path_label))
+        browse_button.grid(column=0, row=1, sticky=(tk.W, tk.E))
+
+        # Frame and window formatting
+
+        frame.columnconfigure(1, weight=1)
+        frame.rowconfigure(1, weight=1)
+        return frame, path_label
+
+    def create_setup_roms_sd_card_frame():
+        frame = ttk.Frame(right_frame)
+        frame.columnconfigure(1, weight=1)
+
+        # Create the path_label
+        path_label = ttk.Label(frame, text="")
+        path_label.grid(column=1, row=2, sticky=tk.W)  # Left-align the label within the grid
+
+        # Configure the columns
+        frame.columnconfigure(0, weight=1)
+        #frame.columnconfigure(1, weight=0)  # Set the weight to 0 for the column containing the label
+        #frame.columnconfigure(2, weight=1)
+
+        # Create Button to create folders for Garlic OS
+        create_folders_for_garlic_os_button = ttk.Button(
+            frame,
+            text="Create Folders for Garlic OS",
+            state=tk.DISABLED,
+            command=lambda: setup_sd_card("Garlic")
+        )
+        create_folders_for_garlic_os_button.grid(column=0, row=3, sticky=(tk.W, tk.E))
+
+        # Create Button to create folders for Batocera
+        create_folders_for_batocera_os_button = ttk.Button(
+            frame,
+            text="Create Folders for Batocera",
+            state=tk.DISABLED,
+            command=lambda: setup_sd_card("Batocera")
+        )
+        create_folders_for_batocera_os_button.grid(column=1, row=3, sticky=(tk.W, tk.E))
+
+        # Create Browse Button
+        browse_button = ttk.Button(
+            frame,
+            text="Browse",
+            command=enable_buttons_after_browsing(
+                path_label,
+                create_folders_for_batocera_os_button,
+                create_folders_for_garlic_os_button
+            ),
+        )
+        browse_button.grid(column=0, row=1, sticky=(tk.W, tk.E))
+
+        return frame, path_label
+
     def close_app():
         window.destroy()
         sys.exit(0)
@@ -150,6 +259,9 @@ def main():
     window.minsize(500, 100)
     window.maxsize(500, 100)
     window.protocol("WM_DELETE_WINDOW", close_app)
+    window.columnconfigure(0, weight=0, minsize=150)  # Set minsize=200 for the left frame
+    window.columnconfigure(1, weight=1)
+    window.rowconfigure(0, weight=1)
     try:
         # PyInstaller creates a temp folder and stores the path in _MEIPASS
         base_path = sys._MEIPASS
@@ -170,75 +282,14 @@ def main():
     application_names = populate_application_names(locals())
 
     # Populate Apps
-    apps_listbox = tk.Listbox(left_frame, height=len(application_names), width=calculate_pixel_width(application_names))
-    for i, application_name in enumerate(application_names):
-        apps_listbox.insert(i, application_name)
-    apps_listbox.bind(
-        "<<ListboxSelect>>",
-        lambda event: on_click(apps_listbox.get(apps_listbox.curselection())),
-    )
-    apps_listbox.grid(column=0, row=1)
 
-    right_frame = ttk.Frame(window, relief="groove", borderwidth=1)
-    right_frame.grid(column=1, row=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+    apps_listbox, right_frame, app_title = populate_apps()
 
-    app_title = ttk.Label(right_frame, text="")
-    app_title.grid(column=0, row=0, columnspan=2, sticky=(tk.W, tk.E))
+    # Create Organize CHD Files Frame
+    chd_frame, chd_path_label = create_organize_chd_files_frame()
 
-    chd_frame = ttk.Frame(right_frame)
-    chd_frame.columnconfigure(1, weight=1)
-
-    browse_button = ttk.Button(chd_frame, text="Browse", command=browse_directory)
-    browse_button.grid(column=0, row=1, sticky=(tk.W, tk.E))
-
-    path_label = ttk.Label(chd_frame, text="")
-    path_label.grid(column=1, row=1, sticky=(tk.W, tk.E))
-
-    organize_button1 = ttk.Button(
-        chd_frame,
-        text="Generate M3U Playlists",
-        state=tk.DISABLED,
-        command=lambda: organize_files("generate_m3u"),
-    )
-    organize_button1.grid(column=0, row=2, sticky=(tk.W, tk.E))
-
-    organize_button2 = ttk.Button(
-        chd_frame,
-        text="Revert to Single Folder",
-        state=tk.DISABLED,
-        command=lambda: organize_files("revert"),
-    )
-    organize_button2.grid(column=1, row=2, sticky=(tk.W, tk.E))
-
-    window.columnconfigure(0, weight=0, minsize=150)  # Set minsize=200 for the left frame
-    window.columnconfigure(1, weight=1)
-    window.rowconfigure(0, weight=1)
-    chd_frame.columnconfigure(1, weight=1)
-    chd_frame.rowconfigure(1, weight=1)
-
-    sd_card_frame = ttk.Frame(right_frame)
-    sd_card_frame.columnconfigure(1, weight=1)
-
-    browse_button_sd_card = ttk.Button(sd_card_frame, text="Browse", command=browse_directory)
-    browse_button_sd_card.grid(column=0, row=1, sticky=(tk.W, tk.E))
-
-    path_label_sd_card = ttk.Label(sd_card_frame, text="")
-    path_label_sd_card.grid(column=1, row=1, columnspan=2, sticky=(tk.W, tk.E))  # Add columnspan=2
-
-    setup_sd_card_button_garlic = ttk.Button(
-        sd_card_frame,
-        text="Create Folders for Garlic OS",
-        state=tk.DISABLED,
-        command=lambda: setup_sd_card("Garlic")
-    )
-    setup_sd_card_button_garlic.grid(column=0, row=2, sticky=(tk.W, tk.E))
-    setup_sd_card_button_batocera = ttk.Button(
-        sd_card_frame,
-        text="Create Folders for Batocera",
-        state=tk.DISABLED,
-        command=lambda: setup_sd_card("Batocera")
-    )
-    setup_sd_card_button_batocera.grid(column=1, row=2, sticky=(tk.W, tk.E))  # Change column from 2 to 1
+    # Create ROM SD Card Setup Frame
+    sd_card_frame, setup_sd_card_folders_path_label  = create_setup_roms_sd_card_frame()
 
     sd_card_frame.grid_remove()  # Hide frame by default
 
